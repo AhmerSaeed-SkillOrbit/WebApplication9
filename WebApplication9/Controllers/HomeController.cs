10129ad36f6d34717f21382ae3a5a399a477e626
Ahmer Saeed <ahmer.saeed@skillorbit.com>
2019-05-18 10:50:59 +0500

    committing

--- WebApplication9/Controllers/HomeController.cs
+++ WebApplication9/Controllers/HomeController.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using WebApplication9.Models;
 
 namespace WebApplication9.Controllers
 {
     public class HomeController : Controller
     {
+        public IConfiguration Configuration { get; }
+        public HomeController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -33,5 +42,40 @@ namespace WebApplication9.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        [HttpPost]
+        public IActionResult Create_Post(Register register)
+        {
+            if (ModelState.IsValid)
+            {
+                string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    //SqlDataReader
+                    connection.Open();
+
+                    string sql = "Select * From Register Where Email = '{register.Email}' AND Password = {register.Password}";
+                    SqlCommand command = new SqlCommand(sql, connection);
+                    using (SqlDataReader dataReader = command.ExecuteReader())
+                    {
+                        //while (dataReader.Read())
+                        //{
+                        //    Teacher teacher = new Teacher();
+                        //    teacher.Id = Convert.ToInt32(dataReader["Id"]);
+                        //    teacher.Name = Convert.ToString(dataReader["Name"]);
+                        //    teacher.Skills = Convert.ToString(dataReader["Skills"]);
+                        //    teacher.TotalStudents = Convert.ToInt32(dataReader["TotalStudents"]);
+                        //    teacher.Salary = Convert.ToDecimal(dataReader["Salary"]);
+                        //    teacher.AddedOn = Convert.ToDateTime(dataReader["AddedOn"]);
+                        //    teacherList.Add(teacher);
+                        //}
+                    }
+                    connection.Close();
+                }
+            }
+            else
+                return View();
+        }
     }
+}
 }
---
+++ WebApplication9/Models/Register.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication9.Models
+{
+    public class Register
+    {   
+        public Register()
+        {
+                
+        }
+
+        [Key]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsActive{ get; set; }
+    }
+}
